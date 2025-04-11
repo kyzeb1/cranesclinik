82c6ab28b4d76a3c89e23f0368f27deba233b276
Kyzeb <204937792+kyzeb1@users.noreply.github.com>
2025-04-11 19:07:47 +0300

    Delete src/main/java/turbomine/battleroyale directory

--- src/main/java/turbomine/battleroyale/Main.java
+++
@@ -1,7 +0,0 @@
-package turbomine.battleroyale;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
--- src/main/java/turbomine/battleroyale/RestartCommand.java
+++
@@ -1,4 +0,0 @@
-package turbomine.battleroyale;
-
-public class RestartCommand {
-}
